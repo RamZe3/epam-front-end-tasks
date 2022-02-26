39bc85afc7e632f8655cb9a9bebc3f481272e070
Ramil Fitkulin <ramil_fitkulin@epam.com>
2022-02-27 03:56:41 +0400

    add logic without img

--- Task_5/js/game/Gameobj.js
+++ Task_5/js/game/Gameobj.js
@@ -1,6 +1,120 @@
-class GameObj{
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  }
+
+const Type = { BLOCK: 'Block', ENEMY: 'Enemy', HERO: 'Hero' , BONUS: 'Bonus'};
+
+class Point{
     constructor(x, y){
         this.x = x
         this.y = y
+    }
+}
+
+class GameObj extends Point{
+    constructor(x, y, type){
+        super(x, y)
+        this.type = type
+        this.lastPoint = new Point(x, y)
+    }
+
+    move(x, y) {
+        this.lastPoint = new Point(this.x, this.y)
+        this.x += x
+        this.y += y
+    }
+}
+
+class GameLogic{
+    constructor(health, enemyDamage, blockDamage, bonusCount){
+        this.health = health
+        this.enemyDamage = enemyDamage
+        this.blockDamage = blockDamage
+        this.bonusCount = bonusCount
+    }
+
+    checkGround(obj){
+        if(obj.x > 9){
+            obj.x = 9
+            this.health -= this.blockDamage
+        }
+        if(obj.x < 0){
+            obj.x = 0
+            this.health -= this.blockDamage
+        }
+        if(obj.y > 9){
+            obj.y = 9
+            this.health -= this.blockDamage
+        }
+        if(obj.y < 0){
+            obj.y = 0
+            this.health -= this.blockDamage
+        }
+    }
+
+    check(hero, gameobj, mass){
+        if(gameobj.type == Type.ENEMY){
+            this.health -= this.enemyDamage
+            hero.x = hero.lastPoint.x
+            hero.y = hero.lastPoint.y
+        }
+        else if(gameobj.type == Type.BLOCK){
+            this.health -= this.blockDamage
+            hero.x = hero.lastPoint.x
+            hero.y = hero.lastPoint.y
+        }
+        else if(gameobj.type == Type.BONUS){
+            this.addBonus(mass)
+            this.bonusCount--
+            gameobj = null
+        }
+    }
+
+    checkGame(){
+        if(this.health <= 0){
+            alert("Вы проиграли(")
+        }
+        if(this.bonusCount == 0){
+            alert("Вы выйграли)")
+        }
+    }
+
+    moveEnemy(mass, enemy){
+        enemy.lastPoint.x = enemy.x
+        enemy.lastPoint.y = enemy.y
+        enemy.x += getRandomInt(-1, 2)
+        enemy.y += getRandomInt(-1, 2)
+
+        if(enemy.x > 9){
+            enemy.x = 9
+        }
+        if(enemy.x < 0){
+            enemy.x = 0
+        }
+        if(enemy.y > 9){
+            enemy.y = 9
+        }
+        if(enemy.y < 0){
+            enemy.y = 0
+        }
+
+        if(mass[enemy.x][enemy.y] == null){
+            mass[enemy.lastPoint.x][enemy.lastPoint.y] = null
+            mass[enemy.x][enemy.y] = enemy
+        }
+        else{
+            enemy.x = enemy.lastPoint.x
+            enemy.y = enemy.lastPoint.y
+        }
+    }
+
+    addBonus(mass){
+        var bonus = new GameObj(getRandomInt(0, 10), getRandomInt(0, 10), Type.BONUS) 
+        while(mass[bonus.x][bonus.y] != null){
+            bonus = new GameObj(getRandomInt(0, 10), getRandomInt(0, 10), Type.BONUS) 
+        }
+        mass[bonus.x][bonus.y] = bonus
     }
 }
--- Task_5/js/script.js
+++ Task_5/js/script.js
@@ -1,31 +1,79 @@
+// Создаем объект изображения
+//var img = new Image();
+
+// Привязываем функцию к событию onload
+// Это указывает браузеру, что делать, когда изображение загружено
+//img.onload = function() {
+//	context.drawImage(img, 10, 10);
+//};
+
+// Загружаем файл изображения
+//img.src = "face.jpg";
+
+var gameLogic = new GameLogic(100, 20, 10, 5)
+var mass = new Array(10);
+    for (var i = 0; i < 10; i++){
+      mass[i] = new Array(10)
+    }
+
+var hero = new GameObj(2, 2, Type.HERO)
+var enemy = new GameObj(4, 4, Type.ENEMY)
+var block = new GameObj(6, 6, Type.BLOCK)
+var bonus = new GameObj(8, 8, Type.BONUS)
+
+mass[2][2] = hero
+mass[4][4] = enemy
+mass[6][6] = block
+mass[8][8] = bonus
+
 function draw() {
     var canvas = document.getElementById('scene');
     if (canvas.getContext) {
       var ctx = canvas.getContext('2d');
     }
-    //ctx.fillStyle = 'rgb(200, 0, 0)';
-        //ctx.fillRect(10, 10, 50, 50);
+    drawGround(ctx, mass)
 
-        //ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-        //ctx.fillRect(30, 30, 50, 50);
-        //var img = new Image();   // Create new img element
-        //img.addEventListener('load', function() {
-        // execute drawImage statements here
-        //}, false);
-        //img.src = '/images/hero.jpg';
-        //ctx.drawImage(img, 0 , 0)
-        //var imgBrick = new Image();
-        //imgBrick.src = 'images/hero.jpg';
-        //ctx.draw(imgBrick)
-    //ctx.drawImage(imgBrick);
-    var mass = new Array(10);
-    for (var i = 0; i < 10; i++){
-      mass[i] = new Array(10)
-    }
-    drawGround(ctx, 0)
-    //for (var i = 1; i < 5; i++){
-      //ctx.fillRect(10*i, 10*i, 50, 50);
-    //}
+    document.addEventListener('keydown', function(event){
+      clear()
+      var obj = new Point(1, 2)
+      switch(event.code){
+        case 'KeyW':
+          console.log('W vveli');
+          hero.move(0, -1)
+          break;
+        case 'KeyS':
+          console.log('S vveli');
+          hero.move(0, 1)
+          break
+        case 'KeyA':
+          console.log('A vveli');
+          hero.move(-1, 0)
+          break
+        case 'KeyD':
+          console.log('D vveli');
+          hero.move(1, 0)
+          break
+      }
+
+      gameLogic.checkGround(hero)
+
+      //gameLogic.addBonus(mass)
+
+      if(mass[hero.x][hero.y]!=null){
+        gameLogic.check(hero, mass[hero.x][hero.y], mass)
+        mass[hero.lastPoint.x][hero.lastPoint.y] = null
+        mass[hero.x][hero.y] = hero
+      }
+      else{
+        mass[hero.lastPoint.x][hero.lastPoint.y] = null
+        mass[hero.x][hero.y] = hero
+      }
+      gameLogic.checkGame()
+      gameLogic.moveEnemy(mass, enemy)
+      //gameLogic.checkGround(enemy)
+      clear()
+      drawGround(ctx, mass)
+    });
   }
 
 function drawGround(ctx, mass){
@@ -34,8 +82,42 @@ function drawGround(ctx, mass){
   ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
   for (var i = 0; i < 10; i++){
     for(var j = 0; j < 10; j++){
-      //var obj = mass[i][j]
-      ctx.fillRect(width*i, height*j, 80, 80)
+      var obj = mass[i][j]
+      if(obj != null){
+        switch (obj.type){
+          case Type.HERO:
+            ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+            ctx.fillRect(width*i, height*j, 80, 80)
+            break
+          case Type.ENEMY:
+            ctx.fillStyle = 'rgb(0, 0, 0)';
+            ctx.fillRect(width*i, height*j, 80, 80)
+            break
+          case Type.BLOCK:
+            ctx.fillStyle = 'rgba(200, 0, 0, 0.5)';
+            ctx.fillRect(width*i, height*j, 80, 80)
+            break
+          case Type.BONUS:
+            ctx.fillStyle = 'rgba(200, 200, 200, 0.5)';
+            ctx.fillRect(width*i, height*j, 80, 80)
+            break
+        }
+      }
+      else{
+        ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.fillRect(width*i, height*j, 80, 80)
+      }
     }
   }
+  const elem = document.querySelector('#info');
+  elem.innerHTML  = '<strong>Health:</strong> ' + gameLogic.health + '<br>' + '<strong>Bonus count:</strong> ' + gameLogic.bonusCount;
+  
 }
+
+function clear() { // очищаем canvas
+  var canvas = document.getElementById('scene');
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
